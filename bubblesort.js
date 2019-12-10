0f43a9506f6c8a37db01e8e10ce46e3c2e501efe
Adam Vare <adam44@me.com>
2019-12-10 13:01:18 -0500

    all for the session

--- bubblesort.js
+++ bubblesort.js
@@ -1,16 +1,44 @@
 function swap(arr, i, j) {
-  if (arr[i] < arr[j]) {
-    let temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-  }
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
 }
 function bubbleSort(array) {
+  //let swapped;
+
   for (let i = 0; i < array.length; i++) {
+    //swapped = false;
+
     for (let j = 0; j < array.length; j++) {
-      swap(array, i, j);
+      if (array[i] < array[j]) {
+        swap(array, i, j);
+        swapped = true;
+      }
     }
+    //if (swapped === false) break;
   }
 
   return array;
 }
+
+// function bubbleSort(arr)
+// {
+
+//    let swapped = Boolean
+//    for (let i = 0; i < n-1; i++)
+//    {
+//      swapped = false;
+//      for (let j = 0; j < n-i-1; j++)
+//      {
+//         if (arr[j] > arr[j+1])
+//         {
+//            swap(&arr[j], &arr[j+1]);
+//            swapped = true;
+//         }
+//      }
+
+//      // IF no two elements were swapped by inner loop, then break
+//      if (swapped == false)
+//         break;
+//    }
+// }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,23 +1,19 @@
-describe('Bubble Sort', function(){
+describe('Bubble Sort', function() {
+  beforeAll(function() {
+    spyOn(window, 'swap').and.callThrough(); // replace existing `window['swap']` method
+  });
+  
 
-  beforeAll(function () {
-    spyOn(tootsiepop, 'lick').and.callThrough(); // replace existing `tootsiepop['lick']` method
+  it('handles an empty array', function() {
+    expect(bubbleSort([])).toEqual([]);
   });
-  it('getting to the center of tootsiepop involves exactly three licks', function () {
-    tootsiepop.getToCenter();
-    expect(tootsiepop.lick.calls.count()).toEqual(3);
+  it('can handle a swap', function() {
+    const testArray = [8, 1, 4, 5, 6];
+    expect(bubbleSort(testArray)).toEqual([1, 4, 5, 6, 8]);
   });
-
-  it('handles an empty array', function(){
-    expect( bubbleSort([]) ).toEqual( [] );
+  it('getting to the center of window involves exactly three swaps', function() {
+    const testArray = [8, 1, 4, 5, 6];
+    window.bubbleSort(testArray);
+    expect(window.swap.calls.count()).toEqual(4);
   });
-  it('can handle a swap', function (){
-    const testArray = [8,1,4,5,6]
-    expect (bubbleSort(testArray)).toEqual([1,4,5,6,8])
-  })
-  it('can handle a 4 swaps', function (){
-    const testArray = [8,1,4,5,6]
-    
-    
-  })
 });
